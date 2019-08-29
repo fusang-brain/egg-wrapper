fc7fbe77c8338c25d7aa9d1cadf8c52c5678f6aa
joson <joson.chou@outlook.com>
2019-08-29 17:57:37 +0800

    feat new

--- src/router.ts
+++
@@ -1,6 +0,0 @@
-import { wrapper } from 'egg-swagger-decorator';
-import { WrapperOptions } from 'egg-swagger-decorator/dist/swaggerJSON';
-
-export default (app: any, options?: WrapperOptions | undefined) => {
-  wrapper(app, options);
-};
